5b9e68cd6fd8bb7b8b6265618374d8693c238c54
JijaroJarosz <alexandre.conceicaogd@gmail.com>
2023-12-09 09:00:16 -0300

    ...

---
+++ aulas/aula_02/main.py
@@ -0,0 +1,28 @@
+# Valor normal
+#n = input('Digite um valor: ')
+
+# Valor sendo convertido pra str
+#n = str(input('Digite um valor: '))
+
+# Valor sendo convertido pra float
+#n = float(input('Digite um valor: '))
+
+# Valor sendo convertido pra booleano
+#n = bool(input('Digite um valor: '))
+
+n = input('Digite algo: ')
+
+#print(n)
+
+# Verifica se é numérico
+#print(n.isnumeric()) 
+
+# Verifica se é alfabético
+#print(n.isalpha())
+
+# Verifica se é alfanumérico 
+#print(n.isalnum())
+
+# verifica se está em maiusculo
+print(n.isupper())
+
